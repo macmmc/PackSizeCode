6f6be152c78d40b391425fc8689fd2e23cfadc4c
Mark Clanton <mark.clanton@outlook.com>
2018-08-08 21:58:02 -0600

    Initial Check-In

---
+++ PackSizeCode/DataClasses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackSizeCode
+{
+    public class DataClasses
+    {
+        public class StartingCoordinate
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public class RootObject
+        {
+            public int InstructionNumber { get; set; }
+            public string Type { get; set; }
+            public StartingCoordinate StartingCoordinate { get; set; }
+            public string TravelDirection { get; set; }
+            public int Length { get; set; }
+        }
+    }
+}
--- PackSizeCode/Program.cs
+++ PackSizeCode/Program.cs
@@ -3,13 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using System.Data;
 
 namespace PackSizeCode
 {
     class Program
     {
+        public static DataTable dtCommands = new DataTable();
         static void Main(string[] args)
         {
+            dtCommands.Columns.AddRange(new DataColumn[1] { new DataColumn("command") });
+            var temp = parseJSON(args[0]);
+            for (int i = 0; i <= temp.Count - 1; i++)
+            {
+                if (temp[i].Type == "Cut")
+                {
+                    dtCommands.Rows.Add("Move cross-head to " + temp[i].StartingCoordinate.X);
+                    dtCommands.Rows.Add("Lower cross-head knife");
+                    if (temp[i].InstructionNumber < 21)
+                    {
+                        dtCommands.Rows.Add("Move cross-head to " + (temp[i + 1].StartingCoordinate.Y));
+                    }
+                    else
+                    {
+                        dtCommands.Rows.Add("Move cross-head to 0");
+                    }
+                    dtCommands.Rows.Add("Raise cross-head knife");
+                }
+                else if (temp[i].Type == "Crease")
+                {
+                    dtCommands.Rows.Add("Move long-head to " + temp[i].StartingCoordinate.X);
+                    dtCommands.Rows.Add("Lower long-head knife");
+                }
+            }
+            var JSON = DataTableToJSON(dtCommands);
+        }
+
+        private static List<DataClasses.RootObject> parseJSON(string v)
+        {
+            List<DataClasses.RootObject> commands = (List<DataClasses.RootObject>)JsonConvert.DeserializeObject(v, typeof(List<DataClasses.RootObject>));
+            return commands;
+        }
+        public static string DataTableToJSON(DataTable dt)
+        {
+            var jsonString = new StringBuilder();
+            if (dt.Rows.Count > 0)
+            {
+                jsonString.Append("[");
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                        jsonString.Append("\"" + dt.Rows[i][j].ToString() + "\"" + "\r\n");
+                }
+                return jsonString.Append("]").ToString();
+            }
+            else
+            {
+                return "[]";
+            }
         }
     }
 }
